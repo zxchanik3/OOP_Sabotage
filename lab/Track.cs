dfb873a42cbe2b788f2af68471d8a77a66485fc7
Андрій Цуз <andr45677@gmail.com>
2025-12-17 18:42:38 +0200

    update TrackSegment

--- lab/Track.cs
+++ lab/Track.cs
@@ -9,30 +9,15 @@ namespace lab
         public int RequiredLapCount { get; set; }
         public double Length { get; set; }
 
-        public double StartLineX { get; set; }
-        public double StartLineY { get; set; }
-
         public int SectorsCount { get; set; }
 
         public List<TrackSegment> Segments { get; set; } = new();
         
-        public Track()
-        {
-            Name = "Default Track";
-            RequiredLapCount = 1;
-            Length = 0.0;
-            StartLineX = 0.0;
-            StartLineY = 0.0;
-            SectorsCount = 0;
-        }
-        public Track(string name, int lapCount, double length, double startX, double startY, int secC)
+        public Track() { }
+        public Track(string name, int lapCount)
         {
             Name = name;
             RequiredLapCount = lapCount;
-            Length = length;
-            StartLineX = startX;
-            StartLineY = startY;
-            SectorsCount = secC;
         }
 
         public double TotalDistance()
@@ -43,19 +28,12 @@ namespace lab
 
         public void DisplayTrackInfo()
         {
-            Console.WriteLine($"\n--- Інформація про Трек ---");
             Console.WriteLine($"Назва: {Name}");
             Console.WriteLine($"Довжина кола: {Length} км");
             Console.WriteLine($"Кількість кіл: {RequiredLapCount}");
             Console.WriteLine($"Секторів: {SectorsCount}");
-            Console.WriteLine($"Координати старту: ({StartLineX}, {StartLineY})");
         }
         
-        public void InitializeSegments() 
-        {
-            Segments.Add(new TrackSegment(SegmentType.StartFinish, 1.0, 1.0, 0, 0.01f)); // Довга пряма
-            Segments.Add(new TrackSegment(SegmentType.Corner, 0.5, 0.4, 150.0, 0.05f)); // Поворот
-            Segments.Add(new TrackSegment(SegmentType.Straight, 2.0, 1.0, 0, 0.01f));  // Пряма
-        }
+     
     }
 }
--- lab/TrackSegment.cs
+++ lab/TrackSegment.cs
@@ -1,29 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace lab
 {
-    public class TrackSegment
+    [JsonDerivedType(typeof(StraightSegment), typeDiscriminator: "Straight")]
+    [JsonDerivedType(typeof(CornerSegment), typeDiscriminator: "Corner")]
+    [JsonDerivedType(typeof(PitLaneSegment), typeDiscriminator: "PitLane")]
+    [JsonDerivedType(typeof(StartFinishSegment), typeDiscriminator: "StartFinish")]
+    public abstract class TrackSegment
     {
         public double Length { get; set; }
-        public SegmentType Type { get; set; }
-        
-        // 1.0 для Straight, 0.5 для Corner, 0.0 для PitLane (тоді Car лише гальмує до MinSpeedLimit)
-        public double AccelerationFactor { get; set; }
-        
-        // якщо це поворот, машина гальмує до цього значення; 
-        // якщо це PitLane, вона не може перевищувати це значення.
-        public double MinSpeedLimit { get; set; } 
-        
-        public float WearFactor { get; set; } // наскільки сильно зношуються шини на цій ділянці, типу на поворотах вище
+        public SegmentType Type { get; set; } 
 
-        public TrackSegment(SegmentType type, double length, double accelFactor, double minSpeedLimit, float wearFactor)
+        public TrackSegment(SegmentType type, double length)
         {
             Type = type;
             Length = length;
-            AccelerationFactor = accelFactor;
-            MinSpeedLimit = minSpeedLimit;
-            WearFactor = wearFactor;
+        }
+
+        // Кожен сегмент сам вирішує, як змінити швидкість машини
+        public abstract void ApplyEffect(Car car, ref double currentSpeed);
+    }
+    
+    public class StraightSegment : TrackSegment
+    {
+        public StraightSegment(double length) : base(SegmentType.Straight, length) { }
+
+        public override void ApplyEffect(Car car, ref double currentSpeed)
+        {
+            double acceleration = car.Acceleration * 2.0; 
+            currentSpeed += acceleration;
+            
+            if (currentSpeed > car.TopSpeed) 
+                currentSpeed = car.TopSpeed;
+        }
+    }
+    
+    public class CornerSegment : TrackSegment
+    {
+        public int Difficulty { get; set; } // 1 - легкий, 3 - важкий
+
+        public CornerSegment(double length, int difficulty) : base(SegmentType.Corner, length)
+        {
+            Difficulty = difficulty;
+        }
+
+        public override void ApplyEffect(Car car, ref double currentSpeed)
+        {
+            // Обмеження швидкості в повороті залежить від шин
+            double gripFactor = car.Tyres.GripLevel / 100.0;
+            
+            double cornerLimit = (300 * gripFactor) / Difficulty; 
+            
+            if (currentSpeed > cornerLimit)
+            {
+                currentSpeed = cornerLimit;
+                // Тут можна додати логіку сильного зносу шин через гальмування
+                car.Tyres.WearDown(); 
+            }
+            car.Tyres.WearDown();
+        }
+    }
+    
+    public class PitLaneSegment : TrackSegment
+    {
+        public PitLaneSegment(double length) : base(SegmentType.PitLane, length) { }
+
+        public override void ApplyEffect(Car car, ref double currentSpeed)
+        {
+            if (currentSpeed > 80) currentSpeed = 80;
+            
+            // Тут можна відновити шини (якщо реалізувати механіку піт-стопу)
+            // car.ChangeTyres(TyreType.Soft); // Приклад
+        }
+    }
+    
+    public class StartFinishSegment : TrackSegment
+    {
+        public StartFinishSegment(double length) : base(SegmentType.StartFinish, length) { }
+
+        public override void ApplyEffect(Car car, ref double currentSpeed)
+        {
+            if (currentSpeed < car.TopSpeed)
+                currentSpeed += car.Acceleration;
         }
     }
 }
